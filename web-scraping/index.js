fc730295078067de36230444bf256fb8f9868da9
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-02-24 00:01:15 +0530

    commit all the changes

--- web-scraping/index.js
+++ web-scraping/index.js
@@ -1,113 +1,54 @@
-// const axios = require("axios");
-// const cheerio = require("cheerio");
+import axios from 'axios'
+import { load } from 'cheerio'
+import { utils, writeFile } from 'xlsx'
 
+const jobs = []
 
-// const extractWebData = async ()=>{
-//     console.log("called");
-//     try {
-//             //the url of the website from which we want to stole the data
-//           const url = "https://www.quikr.com/jobs/job+zwqxj1519612219";  
-
-//           //fetching the skeleton which is HTML of the webpage
-//             const response =  await axios.get(url);
-//             // console.log(response.data);
-
-//             const jobs = [];
-
-//             const $ = cheerio.load(response.data);
-//             $('job-card').each((index, element)=>{
-//                 const job = {};
-//                 console.log(element);
-//                 // job.title = $(element).find('job-title').text().trim();
-//                 // job.company = $(element).find('company-name').text().trim();
-//                 // job.location = $(element).find('.location').text().trim();
-//                 // job.type = $(element).find('.job-type').text().trim();
-//                 // job.postedDate = $(element).find('.posted-date').text().trim();
-//                 // job.description = $(element).find('.job-description').text().trim();
-
-//                 // jobs.push(job);
-//             })
-        
-//             // console.log("Jobs",jobs);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// extractWebData();
-
-
-
-const axios = require("axios");
-const cheerio = require("cheerio");
-const xlsx = require("xlsx");
 /**
- * 1. Get the data from a certain page from amazon - axios, fetch = DONE
- * 2. Filter/ find the required data from the page = DONE
- * 3. Store thoese information into an excel file
+ * Asynchronous function to retrieve data from a specified URL, parse the HTML content, extract job postings, and write the data to an Excel file.
+ *
+ * @return {Promise} The retrieved data from the specified URL, parsed and written to an Excel file.
  */
+const getData = async () => {
+	try {
+		// Getting data from response object sent by axios
+		const { data } = await axios.get(
+			`https://nirzon47.github.io/html-datasets/job-posting-data.html`,
+			{
+				headers: {
+					// Setting content type as html so cheerio can parse it
+					'Content-Type': 'text/html',
+					// User agent for scraping
+					'User-Agent':
+						'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+				},
+			}
+		)
 
-let products = [
-  {
-    name: "Realme c20",
-    price: 20000,
-  },
-  {
-    name: "Apple iPhone",
-    price: 80000,
-  },
-];
+		// Parsing data using cheerio
+		const $ = load(data)
 
-const getDataFromAmazon = async () => {
-  try {
-    const response = await axios.get(
-      "https://www.amazon.com/s?k=phone&page=2&crid=18EUYBSP7O1SQ&qid=1702535235&sprefix=phon%2Caps%2C280&ref=sr_pg_2",
-      {
-        headers: {
-          "Content-Type": "text/html",
-        },
-      }
-    );
-    // console.log(response.data);
-    const $ = cheerio.load(response.data);
-    // const body = $("body");
-    const productCards = $("[data-asin]")
-      .find("span.a-size-medium.a-color-base.a-text-normal") // name
-    //   .find("span.a-price-whole") //price
-    //   .find("span.a-icon-alt") //ratings
-      .each((index, data) => {
-        products.push({ name: $(data).text() });
-        // console.log($(data).text());
-      });
-    //   const body = document.getElementsByTagName("body");
-    //   body[0].querySelector("span.a-size-medium.a-color-base.a-text-normal")
-    //   .map((data) => {
-    //     console.log(data.innerText)
-    //   })
-    // console.log(titleSpans);
+		// Extracting job postings
+		$('.job-card').each((index, element) => {
+			const product = {
+				job_title: $(element).find('.job-title').text(),
+				company_name: $(element).find('.company-name').text(),
+				location: $(element).find('.location').text(),
+				job_type: $(element).find('.job-type').text(),
+				posted_date: $(element).find('.date').text(),
+				description: $(element).find('.job-description').text(),
+			}
 
-    console.log(products);
-    /**
-     * 1. Create a workbook (Excel file)
-     * 2. Insert the data into sheet
-     * 3. Create a sheet (Excel sheet)
-     * 5. Attach the sheet to the file
-     * 5. Write the file to filesystem
-     */
-    const workbook = xlsx.utils.book_new();
-    // const sheetData = [
-    //   ["Name", "Age", "Country"],
-    //   ["John Doe", 25, "USA"],
-    //   ["Jane Doe", 30, "Canada"],
-    //   ["Bob Smith", 22, "UK"],
-    // ];
+			jobs.push(product)
+		})
 
-    // const workSheet = xlsx.utils.aoa_to_sheet(sheetData);
-    const workSheet = xlsx.utils.json_to_sheet(products);
-    xlsx.utils.book_append_sheet(workbook, workSheet, "Sheet1");
-    xlsx.writeFile(workbook, "job.xlsx");
-  } catch (err) {
-    console.log(err);
-  }
-};
+		const workbook = utils.book_new() // Creating a new workbook
+		const worksheet = utils.json_to_sheet(jobs) // Creating a new worksheet with the extracted data
+		utils.book_append_sheet(workbook, worksheet, 'Sheet 1') // Adding the worksheet to the workbook
+		writeFile(workbook, 'job.xlsx') // Writing the workbook to an Excel file
+	} catch (error) {
+		console.error(error)
+	}
+}
 
-getDataFromAmazon();
+getData()
